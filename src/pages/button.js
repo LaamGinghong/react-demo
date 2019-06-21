6d580e3e674918c99d0ae92855f0dfd4a5421493
LaamGinghong <lanjinghang@banggood.com>
2019-06-21 14:54:57 +0800

    增加路由

--- src/pages/button.js
+++ src/pages/button.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 class Button extends React.Component {
+    routeJump(route) {
+        this.props.history.push(route);
+    }
+
     render() {
         return (
             <div>
-                <button></button>
+                <button onClick={() => this.routeJump('/common-state')}>变量提升</button>
             </div>
         )
     }
 }
+
+export default withRouter(Button);
--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,10 +1,21 @@
 import React from "react";
-import Calculator from './route/common-state/calculator';
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import Button from './button';
+
+const calculator = React.lazy(() => import('./route/common-state/calculator'));
 
 function Pages() {
-    return(
+    return (
         <div>
-            <Calculator/>
+            <BrowserRouter>
+                <Button/>
+                <React.Suspense fallback={<div>加载中...</div>}>
+                    <Switch>
+                        <Route exact path='/' render={() => ((<Redirect to='/common-state'/>))}/>
+                        <Route path='/common-state' component={calculator}/>
+                    </Switch>
+                </React.Suspense>
+            </BrowserRouter>
         </div>
     )
 }
