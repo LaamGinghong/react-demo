3221f064c99541763f54f4353731a6ec27070aad
LaamGinghong <abc_351390560@qq.com>
2019-06-23 16:37:46 +0800

    添加fragments短语法

--- src/pages/route/fragments/fragments.js
+++ src/pages/route/fragments/fragments.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Fragments() {
+function Fragments() {//标准写法
     return (
         <React.Fragment>
             相当于ng中的ng-container或者ng-template
@@ -10,6 +10,17 @@ function Fragments() {
         </React.Fragment>
     )
 }
+
+/*function Fragments() { //短语法
+    return(
+        <>
+            相当于ng中的ng-container或者ng-template
+            <Child>123</Child>
+            <Child>456</Child>
+            <Child>789</Child>
+        </>
+    )
+}*/
 
 function Child(props) {
     return(
